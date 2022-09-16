b30d9f2baf9192135d91b2859286778a00e1df0a
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-09-16 16:29:51 +0400

    open details page when click on 'show details' button

---
+++ pages/[meetupId].js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://lp-cms-production.imgix.net/image_browser/Abanotubani-sulfur-baths-tbilisi.jpg?auto=format&q=40&ar=16%3A9&fit=crop&w=1946"
+      title="First Meetup"
+      address="Abashidze 5"
+      description="The first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
