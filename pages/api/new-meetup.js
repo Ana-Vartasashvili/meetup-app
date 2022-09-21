8c959cd25df635e1b0467df45ccc8a3b6c842eb4
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-09-21 21:19:34 +0400

    send post request to the api when submit the form

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    console.log(data);
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://ANA_:EOrzId6XZlzePd0G@cluster0.akzpsar.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    // EOrzId6XZlzePd0G
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    console.log(meetupsCollection);
+
+    await meetupsCollection.insertOne(data);
+
+    return res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
